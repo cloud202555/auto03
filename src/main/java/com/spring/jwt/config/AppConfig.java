b5b400f893862b66ffbaa12b2d76c19afa242eb6
Ashutosh7796 <ashutoshshedgeas87@gmail.com>
2025-03-09 08:06:01 +0530

    ADDED: CORs policy

--- src/main/java/com/spring/jwt/config/AppConfig.java
+++ src/main/java/com/spring/jwt/config/AppConfig.java
@@ -134,7 +134,7 @@ public class AppConfig {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Arrays.asList("https://autocarcares.com", "http://localhost:5173","https://cffffftasting-production.up.railway.app","https://localhost:3000",));
+                config.setAllowedOrigins(Arrays.asList("https://autocarcares.com", "http://localhost:5173","https://cffffftasting-production.up.railway.app","https://localhost:3000"));
                 config.setAllowedMethods(Collections.singletonList("*"));
                 config.setAllowCredentials(true);
                 config.setAllowedHeaders(Collections.singletonList("*"));
